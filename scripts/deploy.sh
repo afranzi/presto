7b67d7a362e5ad817f8624bebc60c3395c623129
Albert Franzi <albert.franzi@schibsted.com>
2018-05-10 14:31:42 +0200

    Add Travis build (#1)
    * Adapt Travis to Schibsted structure
    
    * Add Memory to MavenOpts
    
    * Add Deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -xe
+
+# This script will deploy project packages to artifactory.
+# https://confluence.schibsted.io/display/SPTINF/Bash+helpers+methods+available+in+the+Travis+builds
+source /usr/local/share/bash/travis_dependencies.bash
+
+BASE_PATH=spt/data/sqlaas/presto
+PACKAGE="/tmp/artifacts/presto-server-*.tar.gz"
+
+deploy_ephemeral() {
+    jfrog rt upload ${PACKAGE} generic-ephemeral/${BASE_PATH}/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER}/
+}
+
+deploy_tag() {
+    jfrog rt upload ${PACKAGE} generic-local/${BASE_PATH}/release/${TRAVIS_TAG}/
+}
+
+main() {
+    configure_jfrog_client
+
+    if [ "$TRAVIS_TAG" ]; then
+        deploy_tag
+    else
+        deploy_ephemeral
+    fi
+}
+
+main
+
+
